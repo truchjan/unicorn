7d23cebe93693ad3d34b97b006d07960d53d731c
Jan Truchlý <truchjan@fit.cvut.cz>
2024-05-17 12:12:12 +0200

    update scripts

--- src/main/scripts/db/insert_script.sql
+++ src/main/scripts/db/insert_script.sql
@@ -1,17 +1,17 @@
 insert into link(name, url, image, description, available_firefox, available_chrome, active, new_tab)
-values ('Google', 'www.google.com', 'IMAGE', 'This is google.', 1, 1, 0, 1);
+values ('Google', 'www.google.com', null, 'This is google.', 1, 1, 0, 1);
 
 insert into link(name, url, image, description, available_firefox, available_chrome, active, new_tab)
-values ('Facebook', 'www.facebook.com', 'IMAGE', 'This is facebook.', 1, 1, 1, 0);
+values ('Facebook', 'www.facebook.com', null, 'This is facebook.', 1, 1, 1, 0);
 
 insert into link(name, url, image, description, available_firefox, available_chrome, active, new_tab)
-values ('Youtube', 'www.youtube.com', 'IMAGE', 'Great platform to watch videos.', 1, 1, 1, 0);
+values ('Youtube', 'www.youtube.com', null, 'Great platform to watch videos.', 1, 1, 1, 0);
 
 insert into link(name, url, image, description, available_firefox, available_chrome, active, new_tab)
-values ('Unicorn', 'www.unicorn.com', 'IMAGE', 'Very nice company.', 1, 1, 1, 1);
+values ('Unicorn', 'www.unicorn.com', null, 'Very nice company.', 1, 1, 1, 1);
 
 insert into link(name, url, image, description, available_firefox, available_chrome, active, new_tab)
-values ('Chrome gang', 'www.chromegang.com', 'IMAGE', 'We love chrome.', 0, 1, 1, 1);
+values ('Chrome gang', 'www.chromegang.com', null, 'We love chrome.', 0, 1, 1, 1);
 
 insert into link(name, url, image, description, available_firefox, available_chrome, active, new_tab)
-values ('Firefox crew', 'www.firefoxcrew.com', 'IMAGE', 'Firefox is the best.', 1, 0, 0, 1);
+values ('Firefox crew', 'www.firefoxcrew.com', null, 'Firefox is the best.', 1, 0, 0, 1);
